5ea2c21c58b478c28c1f0e3aa002733f8e92a5ec
Gihan Vimukthi <151061712+Ceeejeey@users.noreply.github.com>
2024-10-22 11:27:11 +0530

    Update #10
    only one admin , once admin signup , signup role in signup page will hide

--- routes/pages.js
+++ routes/pages.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const { pool } = require('../config/config');
 const authenticateToken = require('../middlewares/authMiddleware');
 
 // Basic Route to Test Server
@@ -11,9 +12,20 @@ router.get('/', (req, res) => {
 router.get('/signin', (req, res) => {
     res.render('signin');
 })
-router.get('/signup', (req, res) => {
-    res.render('signup');
-})
+
+router.get('/signup', async (req, res) => {
+    try {
+        // Check if an admin already exists
+        const [adminCheck] = await pool.query('SELECT COUNT(*) AS adminCount FROM users WHERE role = "admin"');
+        const adminExists = adminCheck[0].adminCount > 0; // true if admin exists
+        
+        // Render the signup page and pass adminExists variable
+        res.render('signup', { adminExists });
+    } catch (error) {
+        console.error('Error checking for admin:', error);
+        res.status(500).send('Error loading signup page');
+    }
+});
 
 //staff Dashboard route
 router.get('/dashboards/staff_dashboard', authenticateToken, (req, res) => {
@@ -27,6 +39,17 @@ router.get('/dashboards/staff_dashboard', authenticateToken, (req, res) => {
     });
 });
 
+//admin Dashboard route
+router.get('/dashboards/admin_dashboard', authenticateToken, (req, res) => {
+    if (!req.user) {
+        return res.status(401).json({ message: 'Unauthorized: No user information found' });
+    }
+    
+    console.log(req.user.role);
+    res.render('dashboards/admin_dashboard', {
+        user: req.user // Pass user info to the dashboard
+    });
+});
 //student Dashboard route
 router.get('/dashboards/student_dashboard', authenticateToken, (req, res) => {
     if (!req.user) {
